648b519f982b2ec13df4fa6207528d418ac807c6
IMS <ims@irisshaders.dev>
2024-10-06 05:43:13 +0300

    Upgrade support for Distant Horizons 2.2 (#702)
    * Upgrade support for Distant Horizons 2.2
    
    * upload artifact fix
    
    * Forgot a file
    
    * Apply requested fixes

--- src/main/java/net/irisshaders/iris/Iris.java
+++ src/main/java/net/irisshaders/iris/Iris.java
@@ -45,6 +45,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.loading.LoadingModList;
 import net.minecraftforge.network.NetworkConstants;
 import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFW;
@@ -105,6 +106,8 @@ public class Iris {
 
 	private static String IRIS_VERSION;
 	private static boolean fallback;
+	private static boolean loadPackWhenPossible = false;
+	private static boolean renderSystemInit = false;
 
 	public Iris() {
 		try {
@@ -119,10 +122,13 @@ public class Iris {
 		}
 	}
 
-	public static boolean isPackInUseQuick() {
-		return pipelineManager.getPipelineNullable() instanceof IrisRenderingPipeline;
-	}
+    public static void loadShaderpackWhenPossible() {
+        loadPackWhenPossible = true;
+    }
 
+    public static boolean isPackInUseQuick() {
+        return pipelineManager.getPipelineNullable() instanceof IrisRenderingPipeline;
+    }
 	public void onKeyRegister(RegisterKeyMappingsEvent event) {
 		event.register(reloadKeybind);
 		event.register(toggleShadersKeybind);
@@ -145,8 +151,12 @@ public class Iris {
 
 		PBRTextureManager.INSTANCE.init();
 
+		renderSystemInit = true;
+
 		// Only load the shader pack when we can access OpenGL
-		loadShaderpack();
+		if (LoadingModList.get().getModFileById("distanthorizons") == null) {
+			loadShaderpack();
+		}
 	}
 
 	public static void duringRenderSystemInit() {
@@ -632,6 +642,19 @@ public class Iris {
 	public static PipelineManager getPipelineManager() {
 		if (pipelineManager == null) {
 			pipelineManager = new PipelineManager(Iris::createPipeline);
+		}
+
+		if (loadPackWhenPossible && renderSystemInit) {
+			loadPackWhenPossible = false;
+			try {
+				reload();
+			} catch (IOException e) {
+				logger.error("Error while reloading Shaders for " + MODNAME + "!", e);
+
+				if (Minecraft.getInstance().player != null) {
+					Minecraft.getInstance().player.displayClientMessage(Component.translatable("iris.shaders.reloaded.failure", Throwables.getRootCause(e).getMessage()).withStyle(ChatFormatting.RED), false);
+				}
+			}
 		}
 
 		return pipelineManager;
