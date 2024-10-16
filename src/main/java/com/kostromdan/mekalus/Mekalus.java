99d47127cdbd99c6d6f3042c49562f4f4ccb01fa
KostromDan <kostromdan@gmail.com>
2024-10-16 22:30:30 +0300

    Change names, authors, etc: initial setup of mdk.

---
+++ src/main/java/com/kostromdan/mekalus/Mekalus.java
@@ -0,0 +1,24 @@
+package com.kostromdan.mekalus;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Mekalus.MODID)
+public class Mekalus {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "mekalus";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+}
